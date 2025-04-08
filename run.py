670b75213c57215061edcf1b57c852c3bb11f65a
Michael <avilinmc@gmail.com>
2025-04-07 22:24:35 -0500

    JS character progress update, QOL.

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-# Add the parent directory to sys.path to ensure imports work correctly
+# imports can sometims fail, so use parent directory
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 from app import app
